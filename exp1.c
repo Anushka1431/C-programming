602cafbd0c66dbe3c23e96f7afb220e2206d8d51
Anushka1431 <anushkappawar08@gmail.com>
2023-01-06 17:09:07 +0530

    my first time on github

---
+++ exp1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/// @brief 
+/// @return 
+int main()
+{
+    int num;
+    printf("enter number ",num);
+    scanf("%d",&num);
+    printf("%d is the number you entered!",num);
+    return 0;
+}
